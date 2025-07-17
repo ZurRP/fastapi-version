37ca72407b7c7448f3d97ceac57af0d62a917f3c
ZurRP <zur@reachplayers.com>
2025-07-17 16:11:47 +0300

    Update db.py
    DB to Railway

--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 # db.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://user:password@containers.railway.app:5432/mydatabase"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
