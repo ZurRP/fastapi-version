5ad45cee97e271d1314b85b3b24ab6d84d39b45a
ZurRP <zur@reachplayers.com>
2025-07-17 19:33:42 +0300

    Update db.py

--- db.py
+++ db.py
@@ -3,11 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+import os
 
-engine = create_engine(
-    DATABASE_URL,
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set. Make sure it's defined in the environment.")
+
+engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
